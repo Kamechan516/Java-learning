eaec96faf0531083e9fef257cc215dc4f65f41ed
Kamechan516 <kameharu2003@gmail.com>
2023-06-22 10:49:07 +0900

    コード5-13を少し改造

--- src/Code5_13.java
+++ src/Code5_13.java
@@ -11,7 +11,19 @@ public class Code5_13 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		int[] array = {1, 2, 3};
+		//メソッド実行前に一度出力
+		System.out.println("メソッド実行前");
+		for(int i : array) {
+			System.out.println(i);
+		}
+		System.out.println();
+		
+
+		
 		incArray(array);
+		
+		//メソッド実行後
+		System.out.println("メソッド実行後");
 		for(int i : array) {
 			System.out.println(i);
 		}
