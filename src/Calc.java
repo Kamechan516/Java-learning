81111494cfece013215d44417f17984134c3aef5
Kamechan516 <kameharu2003@gmail.com>
2023-06-29 11:29:11 +0900

    6/29授業終了分

---
+++ src/Calc.java
@@ -0,0 +1,15 @@
+
+public class Calc {
+	
+	//mainメソッド
+	public static void main(String[] args) {
+		//各種変数の生成
+		int a = 10; int b  = 2;
+		int total = CalcLogic.tasu(a, b);
+		int delta = CalcLogic.hiku(a, b);
+		
+		//出力
+		System.out.println("足すと" + total + "、引くと" + delta);
+	}
+}
+	
---
+++ src/CalcLogic.java
@@ -0,0 +1,14 @@
+
+public class CalcLogic {
+	
+	//足し算を行うメソッド
+	public static int tasu(int a, int b) {
+		return(a + b);
+	}
+	
+	//引き算を行うメソッド
+	public static int hiku(int a, int b) {
+		return(a - b);
+	}
+
+}
