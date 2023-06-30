1a7002d185afa638b99a5482cd813e872b5a8fc2
Kamechan516 <kameharu2003@gmail.com>
2023-07-01 00:30:08 +0900

    訂正

--- src/main/Calc.java
+++ src/main/Calc.java
@@ -1,5 +1,5 @@
 package main;
-import logic.CalcLogic;
+//import logic.CalcLogic;
 
 public class Calc {
 	
@@ -7,8 +7,8 @@ public class Calc {
 	public static void main(String[] args) {
 		//各種変数の生成
 		int a = 10; int b  = 2;
-		int total = CalcLogic.tasu(a, b);
-		int delta = CalcLogic.hiku(a, b);
+		int total = logic.CalcLogic.tasu(a, b);
+		int delta = logic.CalcLogic.hiku(a, b);
 		
 		//出力
 		System.out.println("足すと" + total + "、引くと" + delta);
