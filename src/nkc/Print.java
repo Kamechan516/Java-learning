cb0c59b0878a8b1fecef511a3dd16afcfbb2b6aa
Kamechan516 <kameharu2003@gmail.com>
2023-07-06 10:47:23 +0900

    名前空間の問題を追加

---
+++ src/nkc/Print.java
@@ -0,0 +1,8 @@
+package nkc;
+
+public class Print {
+
+	public static void schoolprint() {
+		System.out.println("名古屋工学院専門学校です");
+	}
+}
---
+++ src/nkcInformation/Print.java
@@ -0,0 +1,8 @@
+package nkcInformation;
+
+public class Print {
+
+	public static void schoolPrint() {
+		System.out.println("名古屋工学院専門学校　情報システム科です");
+	}
+}
